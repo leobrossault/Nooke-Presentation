59a4358926d1a9135df0959e8cb7c7fd3f2a63cf
Brossault <brossault.leo@gmail.com>
2015-05-21 23:30:09 +0200

    Start responsiv + img compress

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,5 +2,14 @@ $(document).ready(function() {
     $('#fullpage').fullpage({
     	navigation: true,
     	navigationPosition: 'right',
+    	// onLeave: function(index, nextIndex, direction){
+    	// 	var body = $('body');
+    	// 	for (var i = 0; i < 9; i ++) {
+    	// 		var oldClass = "section-"+i+"active"; 
+    	// 		body.removeClass(oldClass);
+    	// 	}
+    	// 	var classBody = "section-"+index+"active";
+    	// 	body.addClass(classBody);
+    	// }
     });
 });
