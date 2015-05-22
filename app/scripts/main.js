8a6d9984df56089211705cb1083cad80c830a29a
BROSSAULT Leo <lbrossault@p-212b-13.intra.gobelins.fr>
2015-05-22 10:29:28 +0200

    responsiv OK

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,15 +1,49 @@
 $(document).ready(function() {
+    /* VAR */
+    var wDevice = window.innerWidth;
+    var hDevice = window.innerHeight;
+
+    /* FULL PAGE */
     $('#fullpage').fullpage({
     	navigation: true,
     	navigationPosition: 'right',
-    	// onLeave: function(index, nextIndex, direction){
-    	// 	var body = $('body');
-    	// 	for (var i = 0; i < 9; i ++) {
-    	// 		var oldClass = "section-"+i+"active"; 
-    	// 		body.removeClass(oldClass);
-    	// 	}
-    	// 	var classBody = "section-"+index+"active";
-    	// 	body.addClass(classBody);
-    	// }
+        anchors: ['home', 'teaser', 'concept', 'fourthPage', 'fivePage', 'sixPage', 'demo', 'eightPage', 'teamPage'],
     });
+
+    init ();
+
+    if (wDevice <= 680) {
+        mobile ();
+    }
+
+
+    /* INIT */
+    function init () {
+        var moreLink = $("#more");
+
+        moreLink.click(function () {
+            $.fn.fullpage.moveTo(2);
+        }); 
+    }
+
+    /* MOBILE */
+    function mobile () {
+        var menu = $('#menu ul');
+        var burger = $('#burger');
+        var hLink = (hDevice - 59) / 5;
+        var link = $('#menu ul a');
+
+        menu.height(hDevice);
+        link.height(hLink);
+
+        burger.click(function () {
+            if (menu.hasClass('active')) {
+                menu.removeClass('active');
+                $(this).removeClass('active');
+            } else {
+                menu.addClass('active');
+                $(this).addClass('active');
+            }
+        }); 
+    }
 });
